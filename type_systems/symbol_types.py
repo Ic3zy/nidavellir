941f803856d55c6b83f772cb5dcf898fdb08de9b
Ic3zy <abdullah51dundar51@gmail.com>
2026-08-27 19:22:55 +0300

    feat: extract SymbolTreeBuilder, implement AST node tracking, and refactor TypeDefEngine

--- type_systems/symbol_types.py
+++ type_systems/symbol_types.py
@@ -11,24 +11,29 @@ class GlobalScope(Symbol):
 
 
 class VariableSymbol(Symbol):
-    def __init__(self, name: str, type_annotation: str):
+    def __init__(self, name: str, type_annotation: str, ast_node):
         self.name = name
         self.type = type_annotation
+        self.ast_node = ast_node
         self.used_stack = []
 
 
 class FunctionSymbol(Symbol):
-    def __init__(self, name: str, return_type: str, params: list, is_variadic=False):
+    def __init__(
+        self, name: str, return_type: str, params: list, ast_node, is_variadic=False
+    ):
         self.name = name
         self.return_type = return_type
         self.params = params
+        self.ast_node = ast_node
         self.call_stack = []
         self.is_variadic = is_variadic
 
 
 class ClassSymbol(Symbol):
-    def __init__(self, name: str, params: list, scope):
+    def __init__(self, name: str, params: list, scope, ast_node):
         self.name = name
         self.params = params
         self.scope = scope
+        self.ast_node = ast_node
         self.call_stack = []
---
+++ type_systems/tree_builder.py
@@ -0,0 +1,131 @@
+from semantic import INTRINSIC_HANDLERS
+from nida_ast.base import *
+from .symbol_table import ScopeManager
+from .symbol_types import *
+
+
+class SymbolTreeBuilder:
+    def __init__(self, ast_tree):
+        self.ast_tree = ast_tree
+        self.sm = ScopeManager()
+
+    def error(self, node, message):
+        line = getattr(node, "line", None)
+        col = getattr(node, "column", None)
+        loc = f" [Line {line}:{col}]" if line is not None else ""
+        raise SyntaxError(f"Nidavellir Error{loc}: {message}")
+
+    def eval_VariableAST(self, ast):
+        var = self.sm.lookup(ast.name)
+        if var is None:
+            self.error(ast, f"Variable '{ast.name}' is not defined")
+
+        return var
+
+    def eval_NumberAST(self, ast):
+        pass
+
+    def eval_CallAST(self, ast):
+        self.stmt_CallAST(ast)
+
+    def inference_class_params(self, ast):
+        params = []
+        for node in ast.body:
+            if isinstance(node, FunctionAST):
+                for arg in node.args:
+                    type = arg.type_annotation
+                    target = arg.target
+                    params.append({"type": type, "name": target})
+
+        return params
+
+    def stmt_ClassAST(self, ast):
+        params = self.inference_class_params(ast)
+        self.sm.add_symbol(ClassSymbol(ast.name, params, None, ast))
+        self.sm.enter_scope(attach_to_parent=False)
+
+        sym = self.sm.lookup(ast.name)
+        sym.scope = self.sm.current_scope
+
+        for node in ast.body:
+            self.visit_statement(node)
+
+        self.sm.exit_scope()
+
+    def stmt_PassAST(self, ast):
+        pass
+
+    def stmt_FunctionAST(self, ast):
+        self.sm.add_symbol(FunctionSymbol(ast.name, None, ast.args, ast))
+        self.sm.enter_scope()
+        for param in ast.args:
+            self.sm.add_symbol(VariableSymbol(param.target, param.type_annotation, ast))
+
+        for stmt in ast.body:
+            self.visit_statement(stmt)
+
+        self.sm.exit_scope()
+
+    def transform_Intrinsic_to_Symbol(self, node):
+        return FunctionSymbol(
+            node["name"],
+            node["return_type"],
+            node["params"],
+            None,  # ast_node
+            is_variadic=node["is_variadic"],
+        )
+
+    def stmt_CallAST(self, ast):
+        func_name = ast.target
+        if func_name in INTRINSIC_HANDLERS:
+            temp = INTRINSIC_HANDLERS[func_name]
+            func = self.transform_Intrinsic_to_Symbol(temp)
+        else:
+            func = self.sm.get_symbol(ast.target)
+
+        if isinstance(func, FunctionSymbol):
+            if len(ast.args) != len(func.params) and not func.is_variadic:
+                self.error(
+                    ast, f"Function {ast.target} takes {len(func.params)} arguments"
+                )
+
+            for arg in ast.args:
+                var = self.visit_expression(arg)
+                if not isinstance(var, VariableSymbol):
+                    self.error(arg, f"Cannot pass non-variable symbol {arg.name}")
+
+                var.used_stack.append(ast)
+
+        elif isinstance(func, ClassSymbol):
+            if len(ast.args) != len(func.params):
+                self.error(ast, f"Class {ast.target} takes 1 argument")
+
+        func.call_stack.append(ast)
+
+    def stmt_AssignAST(self, ast):
+        self.sm.add_symbol(VariableSymbol(ast.target, ast.type_annotation, ast))
+        self.visit_expression(ast.value)
+
+    def visit_statement(self, node):
+        method_name = f"stmt_{type(node).__name__}"
+        visitor = getattr(self, method_name, None)
+        if visitor is None:
+            self.error(
+                node,
+                f"Standalone expression '{type(node).__name__}' is not a valid statement",
+            )
+        return visitor(node)
+
+    def visit_expression(self, node):
+        method_name = f"eval_{type(node).__name__}"
+        visitor = getattr(self, method_name, None)
+        if visitor is None:
+            self.error(node, f"Invalid expression context for '{type(node).__name__}'")
+
+        return visitor(node)
+
+    def run(self):
+        for node in self.ast_tree:
+            self.visit_statement(node)
+
+        self.sm.print_scopes()
